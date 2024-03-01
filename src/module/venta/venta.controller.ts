df2995521e52e5cdf20cab2ae3c4eb29d947c341
Frederick Torres <fredtor.tor.d@gmail.com>
2024-02-29 23:52:13 -0500

    Mensaje de commit descriptivo

--- src/module/venta/venta.controller.ts
+++ src/module/venta/venta.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { VentaDto } from '../dto/venta.dto';
+import { VentaService } from './venta.service';
 
-@Controller('venta')
-export class VentaController {}
+@Controller('ventas')
+export class VentaController {
+  constructor(private readonly ventaService: VentaService) {}
+
+  @Post()
+  async create(@Body() ventaDto: VentaDto) {
+    return this.ventaService.create(ventaDto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.ventaService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number) {
+    return this.ventaService.findOne(id);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() ventaDto: VentaDto) {
+    return this.ventaService.update(id, ventaDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: number) {
+    return this.ventaService.remove(id);
+  }
+}
+
--- src/module/venta/venta.service.ts
+++ src/module/venta/venta.service.ts
@@ -1,4 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Venta } from '../entities/venta.entity';
+import { VentaDto } from '../dto/venta.dto';
+import { plainToClass } from 'class-transformer'; 
 
 @Injectable()
-export class VentaService {}
+export class VentaService {
+  constructor(
+    @InjectRepository(Venta)
+    private readonly ventaRepository: Repository<Venta>,
+  ) {}
+
+  async create(ventaDto: VentaDto): Promise<Venta> {
+    // Verificar que la fecha de venta no sea mayor que la fecha actual
+    if (ventaDto.fecha_venta > new Date()) {
+      throw new BadRequestException('La fecha de venta no puede ser en el futuro');
+    }
+    // Convertir VentaDto a DeepPartial<Venta> usando plainToClass
+    const venta = plainToClass(Venta, ventaDto);
+    // Crear la venta si la validación pasa
+    return this.ventaRepository.save(venta);
+  }
+
+  async findAll(): Promise<Venta[]> {
+    return this.ventaRepository.find({ relations: ['fk_codigo', 'fk_cliente'] });
+  }
+
+  async findOne(id_venta: number): Promise<Venta | undefined> {
+    return this.ventaRepository.findOne({where:{id_venta}, relations: ['fk_codigo', 'fk_cliente'] } );
+  }
+
+  async update(id_venta: number, ventaDto: VentaDto): Promise<Venta | undefined> {
+    const venta = await this.ventaRepository.findOne({where:{id_venta}});
+    if (!venta) {
+      return undefined;
+    }
+    Object.assign(venta, ventaDto);
+    return this.ventaRepository.save(venta);
+  }
+
+  async remove(id_venta: number): Promise<Venta | undefined> {
+    const venta = await this.ventaRepository.findOne({where:{id_venta}});
+    if (!venta) {
+      return undefined;
+    }
+    await this.ventaRepository.remove(venta);
+    return venta;
+  }
+}
+
