c20051fe6e13ec43a6ea5d0f48893ced33b8df45
Frederick Torres <fredtor.tor.d@gmail.com>
2024-03-11 23:05:10 -0500

    11/03/2024-11:04PM

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AuthComponent } from './components/auth/auth.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AuthComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
